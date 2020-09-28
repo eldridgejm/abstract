97af53450e5152ded593474ff98b477cf74e261b
Justin Eldridge <eldridgejm@gmail.com>
2020-09-28 13:03:51 -0700

    Added pytest runner to setup requires.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name="abstract",
     version="0.1.0",
     packages=find_packages(),
+    setup_requires=["pytest-runner"],
     install_requires=["jinja2", "markdown", "publish"],
     tests_require=["pytest"],
     entry_points={"console_scripts": ["abstract = abstract:cli"]},
