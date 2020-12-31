4ec4daa35a32163c6dbab8cc83fb25bc7b2720eb
Justin Eldridge <eldridgejm@gmail.com>
2020-12-30 23:41:20 -0800

    Version bump.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="abstract",
-    version="0.1.3",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=["jinja2", "pyyaml", "markdown", "publish"],
     entry_points={"console_scripts": ["abstract = abstract:cli"]},
